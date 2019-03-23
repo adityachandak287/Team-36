e6b693dad202b6cb61cb59a5fb28f5d6e00e8dde
Aditya Chandak <adityachandak287@gmail.com>
2019-03-23 10:49:09 +0530

    Added bidding and char portal

---
+++ biddingChat/public/user.js
@@ -0,0 +1,135 @@
+let user = "",
+  userdata = {};
+let sellers = [];
+let socket;
+let sellerRoom;
+let bidData = [];
+let highestBid;
+$().ready(() => {
+  // $("#sellerDiv").hide();
+  // $("#buyerDiv").hide();
+});
+document.getElementById("buyerBtn").addEventListener("click", () => {
+  user = "Buyer";
+  $("#buyerDiv").slideDown();
+  document.getElementById("sellerBtn").disabled = true;
+  connectIO();
+});
+document.getElementById("sellerBtn").addEventListener("click", () => {
+  user = "Seller";
+  document.getElementById("buyerBtn").disabled = true;
+  $("#sellerDiv").slideDown();
+  connectIO();
+});
+
+function connectIO() {
+  if (user) {
+    socket = io.connect("http://localhost:3000");
+    if (user == "Buyer") {
+      socket.on("getSellers", data => {
+        console.log(data.sellers);
+        addSellers(data.sellers, socket);
+      });
+      socket.on("updateSellers", data => {
+        addSellers(data.sellers, socket);
+        console.log(data.sellers);
+      });
+      socket.on("getBids", data => {
+        bidData = data;
+        makeBidLog();
+      });
+      document.getElementById("submitBid").addEventListener("click", () => {
+        let logData = {
+          seller: sellerRoom,
+          buyer: socket.id,
+          bid: document.getElementById("bidAmount").value
+        };
+        bidData.push(logData);
+        console.log(logData);
+        socket.emit("logBid", { bids: bidData, bid: logData });
+        document.getElementById("bidForm").reset();
+      });
+      socket.on("newBid", data => {
+        console.log("New Bid!");
+        bidData.push(data.bid);
+        addBid(data.bid);
+        console.log(data);
+      });
+    } else if (user == "Seller") {
+      socket.emit("createSeller", {
+        name: "Seller" + Math.trunc(Math.random() * 100)
+      });
+      socket.on("getBids", data => {
+        bidData = data;
+        makeBidLog();
+      });
+      socket.on("newBid", data => {
+        console.log("New Bid!");
+        bidData.push(data.bid);
+        addBid(data.bid);
+        console.log(data);
+      });
+    }
+  }
+}
+
+function addSellers(sellerData) {
+  let sellerDiv = document.getElementById("buyerDiv");
+  sellerData.forEach(seller => {
+    if (sellers.indexOf(seller) == -1) {
+      let p = document.createElement("button");
+      p.textContent = seller;
+      p.classList.add("btn");
+      p.classList.add("btn-warning");
+      p.addEventListener("click", () => {
+        socket.emit("joinSeller", { seller: seller });
+        sellerRoom = seller;
+        console.log(seller);
+      });
+      sellerDiv.appendChild(p);
+      sellers.push(seller);
+    }
+  });
+}
+
+function makeBidLog() {
+  let myNode = document.getElementById("bidLog");
+  while (myNode.firstChild) {
+    myNode.removeChild(myNode.firstChild);
+  }
+  document.getElementById("highestBid").textContent = "Highest Bid:";
+  if (bidData.length > 0) {
+    bidData.forEach(bid => {
+      addBid(bid);
+    });
+  }
+}
+
+function addBid(bid) {
+  console.log(bid);
+  let logDiv = document.getElementById("bidLog");
+  let tempDiv = document.createElement("div");
+  let tempH = document.createElement("h6");
+  tempH.textContent = `${bid.buyer} - Rs. ${bid.bid}`;
+  tempDiv.appendChild(tempH);
+  tempDiv.classList.add("bg-dark");
+  tempDiv.classList.add("text-white");
+  tempDiv.classList.add("rounded");
+  tempDiv.classList.add("p-1");
+  tempDiv.classList.add("m-2");
+  logDiv.insertBefore(tempDiv, logDiv.childNodes[0]);
+  highestBid = getHighestBid();
+  document.getElementById("highestBid").textContent = `Highest Bid: ${
+    highestBid.buyer
+  } - Rs.${highestBid.bid}`;
+}
+
+function getHighestBid() {
+  let maxBid = { bid: 0 };
+  bidData.forEach(bid => {
+    if (bid.bid >= maxBid.bid) {
+      maxBid = bid;
+    }
+  });
+  return maxBid;
+}
---
+++ biddingChat/server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var app = express();
+var server = app.listen(3000);
+app.use(express.static("public"));
+console.log("Server is running");
+var socket = require("socket.io");
+var io = socket(server);
+var sellers = [];
+var bidLogs = {};
+io.sockets.on("connection", socket => {
+  socket.emit("getSellers", { sellers: sellers });
+  socket.on("createSeller", data => {
+    sellers.push(data.name);
+    socket.join(data.name);
+    bidLogs[data.name] = [];
+    io.emit("updateSellers", { sellers: sellers });
+    console.log(data.name + " joined");
+  });
+
+  socket.on("joinSeller", data => {
+    var rooms = Object.keys(socket.rooms);
+    if (rooms.length > 1) {
+      console.log(`${socket.id} left room ${rooms[1]}`);
+      socket.leave(rooms[1]);
+    }
+    socket.join(data.seller);
+
+    console.log(rooms);
+    socket.emit("getBids", bidLogs[data.seller]);
+    console.log(`${socket.id} joined room ${data.seller}`);
+  });
+
+  socket.on("logBid", data => {
+    console.log(
+      `${data.bid.bid} from ${data.bid.buyer} in room ${data.bid.seller}`
+    );
+    io.in(data.bid.seller).emit("newBid", data);
+    bidLogs[data.bid.seller].push(data.bid);
+  });
+});
